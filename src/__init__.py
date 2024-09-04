f98ae87a85ca8228081c1ca27f8e8da98b81566e
Ettore Ricci <ettore.ricci.coding@gmail.com>
2024-09-04 02:20:05 +0200

    Adapted for linux

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,4 @@
-PROJECT_ROOT = f"C:/Users/the_E/Documents/APPUNTI/AIDE 1/Data mining and machine learning/Project/"
+import os
+PROJECT_ROOT = "C:/Users/the_E/Documents/APPUNTI/AIDE 1/Data mining and machine learning/Project/"
+if not os.path.exists(PROJECT_ROOT):
+    PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"))
