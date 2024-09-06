4e1dba0da97ee3bcd6c8896a391bbc2ce599914e
Ettore Ricci <minecraft.etto@gmail.com>
2024-09-06 20:24:10 +0200

    Better model search

--- src/models/hyperparameters.py
+++ src/models/hyperparameters.py
@@ -1,4 +1,4 @@
-HYPERPARAMETERS = {
+HYPERPARAMETERS = [{ # Set 0
     "decision_tree": {
         "criterion": ["gini", "entropy", "log_loss"],
         "splitter": ["best"],
@@ -25,7 +25,36 @@ HYPERPARAMETERS = {
         "lr": [1e-1, 1e-2, 1e-3],
         "optimizer": ["adam", "sgd"],
     }
-}
+},
+{ # Set 1
+    "decision_tree": {
+        "criterion": ["gini", "log_loss"],
+        "splitter": ["best"],
+        "max_depth": [1000, None],
+        "min_impurity_decrease": [1e-5, 1e-6, 1e-7],
+        "class_weight": ["balanced"]
+    },
+    "random_forest": {
+        "n_estimators": [100, 125, 150],
+        "criterion": ["gini", "log_loss"],
+        "max_depth": [1000, None],
+        "min_impurity_decrease": [1e-5, 1e-6, 1e-7],
+        "n_jobs": [-1],
+        "class_weight": ["balanced"],
+    },
+    # TODO: Wait for the neural network to finish training before tuning hyperparameters
+    "neural_network": {
+        "network": ["ff_tfidf", "ff_binary", "lstm_embeddings", "cnn_embeddings"],
+        "base_size": [8, 16, 32, 64],
+        "depth": [2, 3, 4],
+        "epochs": [5, 10, 15],
+        "dropout": [0, 0.5],
+        "batchnorm": [True, False],
+        "batch_size": [32],
+        "lr": [1e-1, 1e-2, 1e-3],
+        "optimizer": ["adam", "sgd"],
+    }
+}]
 
 if __name__ == "__main__":
     from itertools import product
--- src/scripts/model_selection.py
+++ src/scripts/model_selection.py
@@ -2,13 +2,19 @@ from dmml_project.dataset import Dataset
 from dmml_project import PROJECT_ROOT
 from dmml_project.models.hyperparameters import HYPERPARAMETERS
 from dmml_project.model_selection.random_search import RandomSearch
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Perform hyperparameter search for models")
+    parser.add_argument("--hyper-set, -h", type=int, help="Index of hyperparameter set to use", default=0)
+    args = parser.parse_args()
+    
     dataset: Dataset = Dataset.load(f"{PROJECT_ROOT}/data/train.tsv")
     
-    for model_kind, hyper_grid in HYPERPARAMETERS.items():
+    for model_kind, hyper_grid in HYPERPARAMETERS[args.hyper_set].items():
         search = RandomSearch(model_kind, hyper_grid, n_iter=10)
-        output_file = f"{PROJECT_ROOT}/data/{model_kind}_search.json"
+        postfix = "" if args.hyper_set == 0 else f"_{args.hyper_set}"
+        output_file = f"{PROJECT_ROOT}/data/{model_kind}_search{postfix}.json"
         result = search.search(dataset, output_path=output_file)
         if result == []:
             exit(1)
