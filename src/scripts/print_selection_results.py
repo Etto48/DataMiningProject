c720c5061d806ec09593dce6d60943783cbe0445
Ettore Ricci <minecraft.etto@gmail.com>
2024-09-08 15:56:39 +0200

    Added wildcards

--- src/scripts/print_selection_results.py
+++ src/scripts/print_selection_results.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 from dmml_project.model_selection.load_results import load_results, get_results_indices, model_name_from_index, index_from_model_name
 import argparse
+import regex as re
 
 def print_config_results(search_results, index):
     result = search_results[index[0]][index[1]][index[2]]
@@ -15,6 +16,10 @@ def print_config_results(search_results, index):
     print(f"Hyperparameters: {json.dumps(hypers, indent=4)}")
     print()
     print(f"Accuracy: {accuracy_avg:.2f} ± {accuracy_std:.2f}")
+
+def names_match(model_name, query) -> bool:
+    query_regex = re.escape(query).replace("\\*", ".*")
+    return re.match(query_regex, model_name) is not None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Print the results of the model selection.")
@@ -32,6 +37,19 @@ if __name__ == "__main__":
             if not args.yes:
                 input("Press Enter to continue...")
             print()
+    elif "*" in args.query:
+        num_matches = 0
+        for index in indices:
+            model_name = model_name_from_index(index)
+            if names_match(model_name, args.query):
+                num_matches += 1
+                print_config_results(search_results, index)
+                print()
+                if not args.yes:
+                    input("Press Enter to continue...")
+                print()
+        if num_matches == 0:
+            print("No matches found.")
     else:
         index = index_from_model_name(args.query, search_results)
         print_config_results(search_results, index)
