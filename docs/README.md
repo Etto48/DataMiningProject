c0654b0f6f2e3a0da244c32186966363f8fff677
Ettore Ricci <minecraft.etto@gmail.com>
2024-09-11 19:48:06 +0200

    Refactored docs paths

---
+++ docs/README.md
@@ -0,0 +1,14 @@
+# Emotion classification from text
+
+Read the [documentation](docs/paper/.aux/main.pdf).
+
+## Installation
+
+```bash
+pip install -e .
+```
+
+## Datasets
+
+- [Crowdflower](https://huggingface.co/datasets/tasksource/crowdflower/blob/main/crowdflower.py)
+- [Emotions](https://www.kaggle.com/nelgiriyewithana/datasets)
--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,9 @@ import regex as re
 PROJECT_ROOT = "C:/Users/the_E/Documents/APPUNTI/AIDE 1/Data mining and machine learning/Project/"
 if not os.path.exists(PROJECT_ROOT):
     PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"))
+DOCS_PATH = os.path.join(PROJECT_ROOT, "docs")
+DOCS_PAPER = os.path.join(DOCS_PATH, "paper")
+PAPER_IMAGES = os.path.join(DOCS_PAPER, "images")
     
 CLASSES = ['worry', 'sadness', 'love', 'happiness', 'surprise', 'anger', 'neutral']
 
