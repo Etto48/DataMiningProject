fabd9940f0ac888280502042763bff9765bf6eb6
Ettore Ricci <minecraft.etto@gmail.com>
2024-09-13 17:50:53 +0200

    New hypers and autodocs

--- src/models/hyperparameters.py
+++ src/models/hyperparameters.py
@@ -15,15 +15,15 @@ HYPERPARAMETERS = [{ # Set 0
         "class_weight": ["balanced"],
     },
     "neural_network": {
-        "network": ["ff_tfidf", "lstm_embeddings", "lstm_glove"],
+        "network": ["ff_tfidf", "lstm_embeddings"],
         "base_size": [8, 16, 32],
         "depth": [1, 2, 3, 4],
-        "epochs": [8, 10, 15],
-        "dropout": [0.25, 0.5],
+        "epochs": [10, 15],
+        "dropout": [0.5],
         "batchnorm": [True, False],
         "batch_size": [32],
-        "lr": [5e-3, 1e-3],
-        "optimizer": ["adam", "sgd"],
+        "lr": [1e-2, 1e-3],
+        "optimizer": ["adam"],
     }
 },
 { # Set 1
@@ -55,8 +55,27 @@ HYPERPARAMETERS = [{ # Set 0
     }
 }]
 
+CAPTIONS = [
+    {
+        "decision_tree": "Decision Trees hyperparameter search space for the first search.",
+        "random_forest": "Random Forest hyperparameter search space for the first search.",
+        "neural_network": "Neural Networks hyperparameter search space for the first search. While using the \\texttt{ff\\_tfidf} network, if \\texttt{batchnorm} is set to \\texttt{True}, the \\texttt{dropout} hyperparameter is set to $0$. When using the \\texttt{lstm\\_embeddings} and \\texttt{lstm\\_glove} networks, the \\texttt{batchnorm} hyperparameter is set to \\texttt{False}."
+    },
+    {
+        "decision_tree": "Decision Trees hyperparameter search space for the second search.",
+        "random_forest": "Random Forest hyperparameter search space for the second search.",
+        "neural_network": "Neural Networks hyperparameter search space for the second search. The same rules described in \\autoref{tab:hyperparameters_neural_network_0} apply."
+    }
+]
+
 if __name__ == "__main__":
     from itertools import product
+    from dmml_project.utils import hyper_range_to_latex
+    from dmml_project import PAPER_TABLES
+    
+    with open(f"{PAPER_TABLES}/hypers.tex", "w") as f:
+        f.write(hyper_range_to_latex(HYPERPARAMETERS, CAPTIONS))
+    
     for n in range(len(HYPERPARAMETERS)):
         total_configs = 0
         for model, params in HYPERPARAMETERS[n].items():
---
+++ src/utils.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from dmml_project import PAPER_TABLES
+from dmml_project.model_selection.load_results import model_name_from_index
+import numpy as np
+from pandas.io.formats.style import Styler
+
+def dataframe_to_latex(df: pd.DataFrame, caption: str, label: str, columns_format: str | None = None, formatters: dict = {}, file: str | None = None) -> str:
+    styler = Styler(df, escape="latex")
+    styler \
+        .hide(axis="index")
+    for column, formatter in formatters.items():
+        styler.format(formatter, subset=column)
+
+    table = styler.to_latex(
+        position="H", 
+        column_format=columns_format, 
+        position_float="centering", 
+        hrules=True,
+    )
+    
+    table = table.replace("\\begin{tabular}", "\\begin{tabularx}{0.48\\textwidth}")
+    table = table.replace("\\toprule","\\hline")
+    table = table.replace("\\midrule","\\hline")
+    table = table.replace("\\bottomrule","\\hline")
+    
+    table = table.replace("\\end{tabular}", 
+        f"\\end{{tabularx}}\n\\caption{{{caption}}}\n\\label{{{label}}}\n"
+    )
+    
+    if file is not None:
+        with open(file, "w") as f:
+            f.write(table)
+    return table
+    
+def single_hyper_to_latex(hypers: dict[str, list], caption: str, label: str) -> str:
+    table = pd.DataFrame(columns=["Hyperparameter", "Search Space"])
+    for key, values in hypers.items():
+        table.loc[len(table)] = [key, values]
+    return dataframe_to_latex(table, caption, label, columns_format="|l|X|", formatters=
+    {
+        "Hyperparameter": lambda x: f"\\texttt{{{x.replace('_','\\_')}}}",
+        "Search Space": lambda x: f"\\{{{", ".join([str(xi).replace("_","\\_") for xi in x])}\\}}"
+    })
+
+def hyper_range_to_latex(hypers: list[dict[str, dict[str, list]]], captions: list[dict[str, str]]) -> str:
+    ret = ""
+    for gen, hyper_gen in enumerate(hypers):
+        for model_kind, hypers in hyper_gen.items():
+            ret += single_hyper_to_latex(hypers, captions[gen][model_kind], f"tab:hyperparameters_{model_kind}_{gen}")
+            ret += "\n\n"
+    return ret
+    
+def best_models_to_latex(best_model_index, best_accuracy, results_indices, similar_models, search_results):
+    # generate table for docs
+    with open(f"{PAPER_TABLES}/best_models.tex", "w") as f:
+        f.write("\\begin{table}[H]\n")
+        f.write("    \\centering\n")
+        f.write("    \\begin{tabular}{|l|r|}\n")
+        f.write("        \\hline\n")
+        f.write("        \\textbf{Model} & \\textbf{Accuracy} \\\\\n")
+        f.write("        \\hline\n")
+        f.write(f"        \\texttt{{{model_name_from_index(results_indices[best_model_index]).replace("_","\\_")}}} & {best_accuracy*100:.2f}\\% \\\\\n")
+        for index in similar_models:
+            gen, model_kind, idx = index
+            accuracy = np.mean(search_results[gen][model_kind][idx][1])
+            f.write(f"        \\texttt{{{model_name_from_index(index).replace("_","\\_")}}} & {accuracy*100:.2f}\\% \\\\\n")
+        f.write("        \\hline\n")
+        f.write("    \\end{tabular}\n")
+        f.write("    \\caption{Best model (first row) and models not significantly different from the best model.}\n")
+        f.write("    \\label{tab:best_models}\n")
+        f.write("\\end{table}\n")
